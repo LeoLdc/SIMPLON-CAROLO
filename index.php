7e3b2a2eb256914d374a65da9afe666a3a3fcee0
Alexsansdraps <a.porhansl@icloud.com>
2018-06-05 16:11:54 +0200

    c

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
       <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
       <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
 
     </head>
@@ -60,11 +61,11 @@
 
       </div>
 
+      <footer><?php include('php/footer.php') ?></footer>
+
 
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-
-<footer><?php include('php/footer.php') ?></footer>
 
     </body>
 
