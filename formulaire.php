c993068039eadaed0fb89b9e8ede1bef5c9d5937
Alexsansdraps <a.porhansl@icloud.com>
2018-06-06 22:11:25 +0200

    maj

--- formulaire.php
+++ formulaire.php
@@ -9,52 +9,78 @@
 	<body>
 
 
-		<div class="container-fluid">
+
+
+		<div id="formulaire" class="container-fluid">
+
 			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-6">
 
-			<div class="col-lg-6 col-md-6 col-sm-6">
 
-				<div class="mx-auto w-50 p-3 border border-danger">
+					<div class="mx-auto w-50 p-3 border border-danger">
 
-					<form action="">
-						<fieldset class="form-group">
-							<label for="email">Nom</label>
-							<input type="text" class="form-control" id="nom" name="nom">
-						</fieldset>
-						<fieldset class="form-group">
-							<label for="email">Prénom</label>
-							<input type="text" class="form-control" id="prenom" name="prenom">
-						</fieldset>
-						<fieldset class="form-group">
-							<label for="email">E-mail</label>
-							<input type="email" class="form-control" id="email" name="email">
-						</fieldset>
-						<fieldset class="form-group">
-							<label for="sujet">Sujet</label>
-							<input type="text" class="form-control" id="sujet" name="sujet">
-						</fieldset>
-						<div class="form-group">
-							<label for="votremessage">Votre message</label>
-							<textarea class="form-control" cols="30" rows="10" id="message" name="message"></textarea>
+						<form action="">
+							<fieldset class="form-group">
+								<label for="email">Nom</label>
+								<input type="text" class="form-control" id="nom" name="nom">
+							</fieldset>
+							<fieldset class="form-group">
+								<label for="email">Prénom</label>
+								<input type="text" class="form-control" id="prenom" name="prenom">
+							</fieldset>
+							<fieldset class="form-group">
+								<label for="email">E-mail</label>
+								<input type="email" class="form-control" id="email" name="email">
+							</fieldset>
+							<fieldset class="form-group">
+								<label for="sujet">Sujet</label>
+								<input type="text" class="form-control" id="sujet" name="sujet">
+							</fieldset>
+							<div class="form-group">
+								<label for="votremessage">Votre message</label>
+								<textarea class="form-control" cols="30" rows="10" id="message" name="message"></textarea>
+							</div>
+							<button type="submit" class="btn btn-danger mx-auto p-6">Envoyer !</button>
+						</form>
+			     </div>
+					</div>
+
+
+					<div class="col-lg-6 col-md-6 col-sm-6">
+						<div class="mx-auto w-50 p-3 border border-danger">
+							<div class="row justify-content-center">
+								<h2>Informations</h2>
+								<div class="col-lg-6 col-md-6 col-sm-6">Lundi <br>
+								Mardi <br>
+							Mercredi <br>
+						Jeudi <br>
+					Vendredi <br> </div>
+									<div class="col-lg-6 col-md-6 col-sm-6">
+										9:00-12:00 14:00-17:30<br>
+									9:00-12:00 14:00-17:30<br>
+								9:00-12:00 14:00-17:30<br>
+							9:00-12:00 14:00-17:30<br>
+						9:00-12:00 14:00-17:30<br>
+					</div>
+
+							</div>
+
 						</div>
-
-						<button type="submit" class="btn btn-danger mx-auto p-6">Envoyer !</button>
-					</form>
-
-				</div>
-
-			</div>
-
-			<div class="col-lg-6 col-md-6 col-sm-6">
-				<div class="mx-auto w-50 p-3 border border-danger">
-					<h1>Informations :</h1>
-				</div>
-			</div>
+					</div>
 
 
-	  </div>
-	</div>
 
+
+
+
+
+
+
+
+
+
+					</div>
+		</div>
 
 
 	</body>
