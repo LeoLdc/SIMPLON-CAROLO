cef279a707d8fb824c5fb3efa885f743f3beddef
Greniert <grenierth@hotmail.fr>
2018-06-06 11:37:01 +0200

    header.php

--- index.php
+++ index.php
@@ -14,54 +14,57 @@
 
       <?php include('php/header.php') ?>
 
-      <div class="container-fluid">
-        <div class="row border">
-          <div class="col-lg-4 col-md-4 col-sm-4 border-right ">
-            <img src="images/img1.png" alt="accueil" width="100%">
-          </div>
-
-          <div class="col-lg-8 col-md-8 col-sm-8">
-            <h2> Présentation courte </h2>
-            <p>Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures. </p>
-          </div>
-        </div>
-        <div class="row border-section bg-dark text-white">
-          <div class="col-lg-12 col-md-12 col-sm-12">
-            <h2>La mentalité Simplon</h2>
-            <h3>Devenir un bon simplonien</h3>
-            <p>Apud has gentes, quarum exordiens initium ab Assyriis ad Nili cataractas porrigitur et confinia Blemmyarum, omnes pari sorte sunt bellatores seminudi coloratis sagulis pube tenus amicti, equorum adiumento pernicium graciliumque camelorum per diversa se raptantes, in tranquillis vel turbidis rebus: nec eorum quisquam aliquando stivam adprehendit vel arborem colit aut arva subigendo quaeritat victum, sed errant semper per spatia longe lateque distenta sine lare sine sedibus fixis aut legibus: nec idem perferunt diutius caelum aut tractus unius soli illis umquam placet.<br>
-
-            Quam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint grassatores, loci iniquitati milites cedunt. ubi autem in planitie potuerint reperiri, quod contingit adsidue, nec exsertare lacertos nec crispare permissi tela, quae vehunt bina vel terna, pecudum ritu inertium trucidantur.</p>
-            <div id="demo" class="collapse coll">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam. Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi meae et meum erga te parentemque tuum beneficium tueri debeo.<br>
-
-              Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.<br>
-
-              Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.<br>
-            </div>
-              <button type="button" class="btn btn-info fix" data-toggle="collapse" data-target="#demo"><i class="fas fa-angle-down"></i></button>
-
-
-          </div>
-        </div>
-        <div class="row border-section">
-          <div class="col-lg-6 col-md-6 col-sm-6">
-            <h2> Comment rejoindre Simplon ? </h2>
-            <button type="button" name="rejoindre">Rejoindre</button>
-          </div>
-
-          <div class="col-lg-6 col-md-6 col-sm-6 border-left bgred">
-            <iframe width="100%" height="315" src="https://www.youtube.com/embed/MPC3gZWyxeo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-          </div>
-
-        </div>
-
+<div class="main">
+  <div class="container-fluid">
+    <div class="row border">
+      <div class="col-lg-4 col-md-4 col-sm-4 border-right ">
+        <img src="images/img1.png" alt="accueil" width="100%">
       </div>
 
-      <footer class="backfooter"><?php include('php/footer.php') ?></footer>
+      <div class="col-lg-8 col-md-8 col-sm-8">
+        <h2> Présentation courte </h2>
+        <p>Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures. </p>
+      </div>
+    </div>
+    <div class="row border-section bg-dark text-white">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <h2>La mentalité Simplon</h2>
+        <h3>Devenir un bon simplonien</h3>
+        <p>Apud has gentes, quarum exordiens initium ab Assyriis ad Nili cataractas porrigitur et confinia Blemmyarum, omnes pari sorte sunt bellatores seminudi coloratis sagulis pube tenus amicti, equorum adiumento pernicium graciliumque camelorum per diversa se raptantes, in tranquillis vel turbidis rebus: nec eorum quisquam aliquando stivam adprehendit vel arborem colit aut arva subigendo quaeritat victum, sed errant semper per spatia longe lateque distenta sine lare sine sedibus fixis aut legibus: nec idem perferunt diutius caelum aut tractus unius soli illis umquam placet.<br>
+
+        Quam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint grassatores, loci iniquitati milites cedunt. ubi autem in planitie potuerint reperiri, quod contingit adsidue, nec exsertare lacertos nec crispare permissi tela, quae vehunt bina vel terna, pecudum ritu inertium trucidantur.</p>
+        <div id="demo" class="collapse coll">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam. Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi meae et meum erga te parentemque tuum beneficium tueri debeo.<br>
+
+          Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.<br>
+
+          Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.<br>
+        </div>
+          <button type="button" class="btn btn-info fix" data-toggle="collapse" data-target="#demo"><i class="fas fa-angle-down"></i></button>
+
+
+      </div>
+    </div>
+    <div class="row border-section">
+      <div class="col-lg-6 col-md-6 col-sm-6">
+        <h2> Comment rejoindre Simplon ? </h2>
+        <button type="button" name="rejoindre">Rejoindre</button>
+      </div>
+
+      <div class="col-lg-6 col-md-6 col-sm-6 border-left bgred">
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/MPC3gZWyxeo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </div>
+
+    </div>
+
+  </div>
+
+  <footer class="backfooter"><?php include('php/footer.php') ?></footer>
+</div>
+
 
 
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
