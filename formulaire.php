984c5eff99a19bd1837f89ccd0c82af34311755e
Alexsansdraps <a.porhansl@icloud.com>
2018-06-06 10:59:00 +0200

    c

---
+++ formulaire.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<title></title>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="form-group">
+				<label for="nom">Nom<input type="text" id="nomform" class="form-control"></label>
+			</div>
+			<div class="form-group">
+				<label for="prenom">Prénom<input type="text" id="prenomform" class="form-control"></label>
+			</div>
+			<div class="form-group">
+				<label for="email">E-mail<input type="email" id="emailform" class="form-control"></label>
+			</div>
+			<div class="form-group">
+				<label for="sujet">Sujet<input type="text" id="sujetform" class="form-control"></label>
+			</div>
+			<div class="form-group">
+				<label for="message">Votre message</label>
+				<textarea rows="5" id="message" class="form-control"></textarea>
+			</div>
+		</div>
+	</body>
+</html>
